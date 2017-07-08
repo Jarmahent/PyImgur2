a0905937e76e4e1afbaa1fe781ca8fe8831a0f25
Kevin <kjh7796@gmail.com>
2017-07-07 21:40:18 -0400

    Removed Files

--- test.py
+++
@@ -1,7 +0,0 @@
-import pyimgur
-
-r = pyimgur.Imgur(
-    client_id="ca8c45141727850",
-    client_secret="6d286bf906a36e8763dc2d4acc525276b047944a"
-)
-r.get_image(id="eZ6CCG0").download(path="", name="test", overwrite=False, size=None)
